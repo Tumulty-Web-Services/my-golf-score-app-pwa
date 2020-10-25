7666b7bd4aacb7c11940fe79b614de39f0c79296
Peter F. Tumulty <ptumulty@sh-law.com>
2020-10-25 13:18:22 -0400

    added yourScore drop down list to utils

---
+++ app/utils/dropDownData.ts
@@ -0,0 +1,54 @@
+import { RadioValue } from '../interfaces'
+
+export const yourScore: RadioValue[] = [
+  {
+    text: '1',
+    label: 'one',
+    name: 'yourScore',
+  },
+  {
+    text: '2',
+    label: 'two',
+    name: 'yourScore',
+  },
+  {
+    text: '3',
+    label: 'three',
+    name: 'yourScore',
+  },
+  {
+    text: '4',
+    label: 'four',
+    name: 'yourScore',
+  },
+  {
+    text: '5',
+    label: 'five',
+    name: 'yourScore',
+  },
+  {
+    text: '6',
+    label: 'six',
+    name: 'yourScore',
+  },
+  {
+    text: '7',
+    label: 'seven',
+    name: 'yourScore',
+  },
+  {
+    text: '8',
+    label: 'eight',
+    name: 'yourScore',
+  },
+  {
+    text: '9',
+    label: 'nine',
+    name: 'yourScore',
+  },
+  {
+    text: '10',
+    label: 'ten',
+    name: 'yourScore',
+  },
+]
