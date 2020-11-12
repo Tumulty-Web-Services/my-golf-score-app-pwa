209a917dc190a0947d42518d915578398baba1f4
Peter F. Tumulty <ptumulty@sh-law.com>
2020-11-12 10:32:14 -0500

    adding post-formatted updates from previous commit

--- utils/signup.js
+++ utils/signup.js
@@ -9,34 +9,38 @@ async function signUp(account) {
     email: email,
     nickname: nickname,
     password: password,
-    picture: picture
-  };
+    picture: picture,
+  }
   const webAuth = new auth0.WebAuth({
     domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENTID
-  });
+    clientID: process.env.AUTH0_CLIENTID,
+  })
 
-  webAuth.signup({ 
-    connection: 'CONNECTION', 
-    email: email, 
-    password: password,
-    username: nickname,
-    name: name,
-    nickname: nickname,
-    picture: picture
-  }, (err) => {
-    if(err) {
-      return {
-        status: 500,
-        message: 'Error! There was an error on our end. Please contact support@golfjournal.io'
-      }
-    } else {
-      return {
-        status: 201,
-        message: 'Success! Welcome to Golf Journal'
+  webAuth.signup(
+    {
+      connection: 'CONNECTION',
+      email: email,
+      password: password,
+      username: nickname,
+      name: name,
+      nickname: nickname,
+      picture: picture,
+    },
+    (err) => {
+      if (err) {
+        return {
+          status: 500,
+          message:
+            'Error! There was an error on our end. Please contact support@golfjournal.io',
+        }
+      } else {
+        return {
+          status: 201,
+          message: 'Success! Welcome to Golf Journal',
+        }
       }
     }
-  })
+  )
 
   return newUser
 }
