f9d29416d4175d822d36458cc446ffff21798711
Peter F. Tumulty <ptumulty@sh-law.com>
2020-11-12 10:01:17 -0500

    pushing formatted version of previous commit

--- test/testutils.js
+++ test/testutils.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { SWRConfig } from "swr";
+import React from 'react'
+import { render } from '@testing-library/react'
+import { SWRConfig } from 'swr'
 
 const Wrapper = ({ children }) => {
-  return <SWRConfig value={{ dedupingInterval: 0 }}>{children}</SWRConfig>;
-};
+  return <SWRConfig value={{ dedupingInterval: 0 }}>{children}</SWRConfig>
+}
 
 const customRender = (ui, options) =>
-  render(ui, { wrapper: Wrapper, ...options });
+  render(ui, { wrapper: Wrapper, ...options })
 
 // re-export everything
-export * from "@testing-library/react";
+export * from '@testing-library/react'
 
 // override render method
-export { customRender as render };
+export { customRender as render }
