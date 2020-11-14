7d8301ef9706dee2dbf4aa2ecdd5cec067e9144d
ptums <ptumulty923@gmail.com>
2020-11-14 13:41:05 -0500

    adding a new env var for stripe

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = withFonts({
     AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
     AUTH0_DB_CONNECTION: process.env.AUTH0_DB_CONNECTION,
     BASE_URL: process.env.BASE_URL,
-    STRIPE_API_ID: process.env.STRIPE_API_ID
+    STRIPE_API_ID: process.env.STRIPE_API_ID,
   },
 })
