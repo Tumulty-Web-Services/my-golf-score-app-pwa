6897582662d7a1028561dd1ef39620874b11424d
ptums <ptumulty923@gmail.com>
2021-01-15 13:48:53 -0500

    removed auth from profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,32 +1,25 @@
 import Head from 'next/head'
-import { useUser } from '../utils/passport/hooks'
 import UserProfile from '../components/UserProfile'
 import CourseHistory from '../components/CourseHistory'
 import styles from '../styles/Profile.module.css'
 
 function Profile() {
-  const user: any = useUser({ redirectTo: '/' })
-
   return (
     <div className={styles.container}>
       <Head>
         <title>My Golf Score - Profile</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {user && (
-        <>
-          <div className={styles.userContainer}>
+      <div className={styles.userContainer}>
             <UserProfile
               path="/"
               profile={{
-                name: user.name,
-                username: user.username,
+                name: "Mike Michigan",
+                username: "mmichigan@gmail.com",
               }}
             />
           </div>
-          <CourseHistory user={user.username} />
-        </>
-      )}
+          <CourseHistory user="mmichigan@gmail.com" />
     </div>
   )
 }
